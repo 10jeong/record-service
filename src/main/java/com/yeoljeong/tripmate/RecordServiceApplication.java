ea13574e22d0ca06c6c54bb755748474bc2a3bd5
0000yuyu <yujin09219@naver.com>
2026-05-03 01:50:52 +0900

    feat : 피드 생성 엔드포인트 연결

--- src/main/java/com/yeoljeong/tripmate/RecordServiceApplication.java
+++ src/main/java/com/yeoljeong/tripmate/RecordServiceApplication.java
@@ -2,8 +2,12 @@ package com.yeoljeong.tripmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@ConfigurationPropertiesScan
 public class RecordServiceApplication {
 
   public static void main(String[] args) {
---
+++ src/main/java/com/yeoljeong/tripmate/record/presentaion/dto/response/FeedCreateResponse.java
@@ -0,0 +1,31 @@
+package com.yeoljeong.tripmate.record.presentaion.dto.response;
+
+import com.yeoljeong.tripmate.record.application.dto.result.FeedCreateResult;
+import com.yeoljeong.tripmate.record.domain.constants.VisibilityType;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import lombok.Builder;
+
+@Builder
+public record FeedCreateResponse(
+    UUID planUnitId,
+    UUID feedId,
+    List<String> imageUrls,
+    VisibilityType visibilityType,
+    String title,
+    String description,
+    LocalDateTime createAt
+) {
+
+  public static FeedCreateResponse from(FeedCreateResult result) {
+    return FeedCreateResponse.builder()
+        .planUnitId(result.planUnitId())
+        .feedId(result.feedId())
+        .imageUrls(result.imageUrls())
+        .visibilityType(result.visibilityType())
+        .title(result.title())
+        .description(result.description())
+        .createAt(result.createAt()).build();
+  }
+}
