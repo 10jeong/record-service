79921c031fb84dfb1c2dd389b32392516f9f4b23
0000yuyu <yujin09219@naver.com>
2026-05-03 03:01:29 +0900

    feat : 서비스 로직 추가

--- src/main/java/com/yeoljeong/tripmate/record/service/command/RecordCommandService.java
+++ src/main/java/com/yeoljeong/tripmate/record/service/command/RecordCommandService.java
@@ -1,9 +1,13 @@
 package com.yeoljeong.tripmate.record.service.command;
 
 import com.yeoljeong.tripmate.record.application.dto.command.FeedCreateCommand;
+import com.yeoljeong.tripmate.record.application.dto.command.FeedVisibilityCommand;
+import com.yeoljeong.tripmate.record.application.dto.result.FeedBaseResult;
 import com.yeoljeong.tripmate.record.application.dto.result.FeedCreateResult;
 
 public interface RecordCommandService {
 
   FeedCreateResult createFeedImage(FeedCreateCommand command);
+
+  FeedBaseResult updateFeedVisibility(FeedVisibilityCommand command);
 }
