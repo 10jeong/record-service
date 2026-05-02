e0b80e3eb623d144a1291bdbdb44fcb77e77bc1e
0000yuyu <yujin09219@naver.com>
2026-05-03 03:01:13 +0900

    feat : entity 및 레포 함수 추가

--- src/main/java/com/yeoljeong/tripmate/record/domain/repository/RecordRepository.java
+++ src/main/java/com/yeoljeong/tripmate/record/domain/repository/RecordRepository.java
@@ -1,8 +1,12 @@
 package com.yeoljeong.tripmate.record.domain.repository;
 
 import com.yeoljeong.tripmate.record.domain.model.Feed;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface RecordRepository {
 
   Feed saveForFeed(Feed feed);
+
+  Optional<Feed> findFeedDataById(UUID feedId);
 }
