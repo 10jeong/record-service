610458b7e10ddf72be9b7acca5b7e72cd63b7c53
0000yuyu <yujin09219@naver.com>
2026-05-03 14:43:04 +0900

    feat : feignClient 처리 서비스 로직 추가

---
+++ src/main/java/com/yeoljeong/tripmate/record/infrastructure/external/PlanAdapter.java
@@ -0,0 +1,37 @@
+package com.yeoljeong.tripmate.record.infrastructure.external;
+
+import com.yeoljeong.tripmate.record.application.client.PlanClient;
+import com.yeoljeong.tripmate.record.infrastructure.external.dto.PlanParticipationResponse;
+import feign.FeignException;
+import java.util.Objects;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PlanAdapter implements PlanClient {
+
+  private static final Logger log = LogManager.getLogger(PlanAdapter.class);
+  private final PlanFeignClient planFeignClient;
+
+  @Override
+  public boolean validateGroupMember(UUID userId, UUID planUnitId) {
+    try {
+      PlanParticipationResponse planParticipationResponse = planFeignClient.getPlanParticipation(
+          planUnitId, userId);
+      if (Objects.equals(planParticipationResponse.userId(), userId)
+          && Objects.equals(planParticipationResponse.status(), "JOINED")) {
+        return true;
+      }
+      log.info(planParticipationResponse);
+    } catch (FeignException e) {
+      log.info(e.getMessage());
+      return false;
+    }
+    return false;
+  }
+}
+
